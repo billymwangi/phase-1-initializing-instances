8e654b8743c17e9be3177bae392c8cba77efaaea
billymwangi <billy.mwangi@student.moringaschool.com>
2022-07-06 16:36:47 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here
+class Breakfast{
+    constructor (food,drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+const breakfast = new Breakfast("bread", "porridge");
+
+class Lunch{
+    constructor(salad, soup, drink){
+        this.salad= salad;
+        this.soup = soup;
+        this.drink = drink
+    }
+}
+const newlunch = new Lunch("tomato salad", "broccoli cheddar soup", "water" );
+
+
+class Dinner{
+    #dessert;
+    constructor(salad, soup, entree, dessert){
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+        this.#dessert = dessert;
+    }
+}
+const newdinner = new Dinner( "side salad", "soup", "steak", "chocolate cake");
